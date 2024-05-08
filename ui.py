35a65be8c6f621112e6a63549e75d80988ac0ebd
Mohammad Malaeb <m.malaeb21@gmail.com>
2024-05-08 20:15:42 +0300

    add true and false buttons

--- ui.py
+++ ui.py
@@ -15,8 +15,15 @@ class QuizInterface:
         self.score_label.grid(row=0, column=1)
 
         self.canvas = Canvas(width=300, height=250)
-        self.canvas.create_text(150, 125, text="Hello World",
-                                font=("Ariel", 20, "italic"))
+        self.canvas.create_text(150, 125, text="Hello World", font=("Ariel", 20, "italic"))
         self.canvas.grid(row=1, column=0, columnspan=2, pady=50)
+
+        true_img = PhotoImage(file="true.png")
+        self.true_btn = Button(image=true_img, highlightthickness=0)
+        self.true_btn.grid(row=2, column=0)
+
+        false_img = PhotoImage(file="false.png")
+        self.false_btn = Button(image=false_img, highlightthickness=0)
+        self.false_btn.grid(row=2, column=1)
 
         self.window.mainloop()
